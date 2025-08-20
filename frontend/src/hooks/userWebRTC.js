b8e175f5cebad34066066db55ac14da3051aab1c
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2025-08-21 01:14:32 +0300

    stun

--- frontend/src/hooks/userWebRTC.js
+++ frontend/src/hooks/userWebRTC.js
@@ -2,6 +2,16 @@ import { useEffect, useRef, useState } from 'react';
 
 const configuration = {
     iceServers: [
+        // Публічні STUN сервери для резерву
+        { urls: 'stun:stun.l.google.com:19302' },
+        { urls: 'stun:stun1.l.google.com:19302' },
+        { urls: 'stun:stun2.l.google.com:19302' },
+        { urls: 'stun:stun3.l.google.com:19302' },
+        { urls: 'stun:stun4.l.google.com:19302' },
+        { urls: 'stun:stun.stunprotocol.org:3478' },
+        { urls: 'stun:stun.voiparound.com' },
+        { urls: 'stun:stun.voipbuster.com' },
+        { urls: 'stun:stun.voipstunt.com' },
         {
             urls: [
                 'stun:185.233.47.117:3478',
@@ -12,7 +22,7 @@ const configuration = {
             credential: 'strongpassword'
         }
     ],
-    iceTransportPolicy: "relay"
+    iceTransportPolicy: "all"
 };
 
 export function useWebRTC(socket, randomId) {
